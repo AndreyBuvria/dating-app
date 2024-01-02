a91b16b4c7c6456dd58cc662e28ff47d52b83553
AndreyBuvria <buvrya228@gmail.com>
2024-01-02 19:42:31 +0100

    feat: login

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -3,7 +3,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'dating-app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss', '../../auth.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected isPasswordHidden: boolean = true;
+}
--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-const MAT_MODULES = [MatToolbarModule, MatButtonModule, MatIconModule];
+const MAT_MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+];
 
 @NgModule({
   imports: [MAT_MODULES],
