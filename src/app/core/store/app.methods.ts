7856ae475387a1dc9d34115fd702d45986aced76
AndreyBuvria <buvrya228@gmail.com>
2024-01-03 18:45:51 +0100

    build: adjust eslint & isntall ngrx/store

--- src/app/core/store/app.methods.ts
+++
@@ -1,8 +0,0 @@
-import { signalStoreFeature, type, withMethods } from '@ngrx/signals';
-import { AppState } from './app.state';
-
-export const withAppMethods = () =>
-  signalStoreFeature(
-    { state: type<AppState>() },
-    withMethods((state) => ({}))
-  );
--- src/app/core/store/app.store.ts
+++
@@ -1,9 +0,0 @@
-import { signalStore, withState } from '@ngrx/signals';
-import { withAppMethods } from './app.methods';
-import { AppState, initialState } from './app.state';
-
-export const AppStore = signalStore(
-  { providedIn: 'root' },
-  withState<AppState>(initialState),
-  withAppMethods()
-);
